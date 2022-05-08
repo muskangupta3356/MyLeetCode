f15196309d91727adeb98de6b5db4d9c069cc81c
muskangupta3356 <muskangupta270798@gmail.com>
2022-05-08 23:31:48 +0530

    Time: 59 ms (10.23%), Space: 14.2 MB (44.62%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    bool isCycle(unordered_map<int,vector<int>> &mp, int* state,int t)
+    {
+        if(state[t]==2)
+            return true;
+        
+         state[t]=2;
+        if(mp.find(t)!=mp.end())
+        {
+           for(int i=0;i<mp[t].size();i++)
+        {
+            if(state[mp[t][i]]!=1)
+            {
+                if(isCycle(mp,state,mp[t][i]))
+                    return true;
+            }
+        } 
+        }
+        
+        state[t]=1;
+        return false;
+    }
+    
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+       unordered_map<int,vector<int>>mp;
+        
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        int *state = new int[numCourses];
+        
+        for(int i=0;i<numCourses;i++)
+        {
+            state[i]=0;
+        }
+        
+        for(int i=0;i<numCourses;i++)
+        {
+            if(state[i]==0)
+            {
+              if(isCycle(mp,state,i))  
+                return false;
+            }
+        }
+        return true;
+    }
+};
