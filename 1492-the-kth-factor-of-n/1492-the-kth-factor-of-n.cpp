be0bb39b7678d7cf5ebcb7269d1931c4f3c85ba6
muskangupta3356 <muskangupta270798@gmail.com>
2022-06-02 20:51:06 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.56%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-       set<int>v; int count=0;
-        int s=sqrt(n);
-        for(int i=1;i<=s;i++)
-        {
-            if(n%i==0)
-            {
-               int t =n/i;
-                v.insert(i);
-                v.insert(t);
-            }
+//        set<int>v; int count=0;
+//         int s=sqrt(n);
+//         for(int i=1;i<=s;i++)
+//         {
+//             if(n%i==0)
+//             {
+//                int t =n/i;
+//                 v.insert(i);
+//                 v.insert(t);
+//             }
             
-        }
+//         }
       
-        if(v.size()<k)
-            return -1;
-        else
-        {
-            for (auto it = v.begin(); it != v.end(); it++)
-            {
-                count++;
-                if(count==k)
-                    return (*it);
-            }
-        }
-        return -1;
+//         if(v.size()<k)
+//             return -1;
+//         else
+//         {
+//             for (auto it = v.begin(); it != v.end(); it++)
+//             {
+//                 count++;
+//                 if(count==k)
+//                     return (*it);
+//             }
+//         }
+//         return -1;
+         float root = sqrt(n);
+
+    for (int i=1; i<root; i++) {
+        if (n % i == 0 && --k == 0) return i;
+    }
+
+    for (int i=root; i>0; i--) {
+        if (n % i == 0 && --k == 0) return n/i;
+    }
+
+    return -1;
     }
 };
