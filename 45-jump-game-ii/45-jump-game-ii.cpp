fde7507adff75cbf481425cd09321ce12f59d0f4
muskangupta3356 <muskangupta270798@gmail.com>
2022-06-26 14:03:37 +0530

    Time: 28 ms (59.40%), Space: 16.5 MB (96.54%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        long *dp = new long[n];
-        dp[n-1]=0;
-        for(int i=0;i<=n-2;i++)
+        // int n = nums.size();
+        // long *dp = new long[n];
+        // dp[n-1]=0;
+        // for(int i=0;i<=n-2;i++)
+        // {
+        //     dp[i]=INT_MAX;
+        // }
+        // for(int j=n-2;j>=0;j--)
+        // {
+        //     for(int i=1;i<=nums[j];i++)
+        //     {   if(j+i < n)
+        //         dp[j]= min(dp[j],1+dp[j+i]);
+        //     }
+        // }
+        // return dp[0];
+        int jumps=0;
+        int current=0;
+        int farthest=0;
+        for(int i=0;i<nums.size()-1;i++)
         {
-            dp[i]=INT_MAX;
-        }
-        for(int j=n-2;j>=0;j--)
-        {
-            for(int i=1;i<=nums[j];i++)
-            {   if(j+i < n)
-                dp[j]= min(dp[j],1+dp[j+i]);
+            farthest= max(farthest,i+nums[i]);
+            if(i==current)
+            {
+                jumps++;
+                current=farthest;
             }
         }
-        return dp[0];
+        return jumps;
     }
 };
