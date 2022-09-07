cef754f755fccb32a7a8b5940321a36f3780ffcc
muskangupta3356 <muskangupta270798@gmail.com>
2022-09-08 00:55:04 +0530

    Time: 3 ms (77.02%), Space: 7.5 MB (41.20%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-
-bool PredictTheWinner(vector<int>& a) {
-    int n=a.size();
-    vector<vector<int>> dp(n+2,vector<int>(n+2,0));
-    for(int i=n-1;i>=0;i--){
-        for(int j=0;j<n;j++){
-            if(i>j) continue;
-            if(i==j){
-                dp[i][j]=a[i];
-            }
-            else if(abs(i-j)==1){
-                dp[i][j]=max(a[i],a[j]);
-            }
-            else{
-                int id=a[i]+min(dp[i+2][j],dp[i+1][j-1]);
-                int ie=a[j]+min(dp[i+1][j-1],dp[i][j-2]);
-                dp[i][j]=max(id,ie);
+    bool PredictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        pair<int,int> **dp = new pair<int,int>* [n];
+        for(int i=0;i<n;i++)
+        {
+            dp[i]= new pair<int,int> [n];
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)
+                    dp[i][j]= make_pair(nums[i],0);
+                else
+                    dp[i][j]=make_pair(0,0);       
             }
         }
+        
+        for(int k=1;k<n;k++)
+        {
+            int i=0;int j=i+k;
+            while(j<n)
+            { 
+                int sum=0;
+                for(int l=i;l<=j;l++)
+                    sum+=nums[l];
+                int temp= max(nums[i]+dp[i+1][j].second,nums[j]+dp[i][j-1].second);
+                dp[i][j]=make_pair(temp,sum-temp);
+                
+                i++;
+                j++;
+            }
+        }
+        if(dp[0][n-1].first>=dp[0][n-1].second)
+            return true;
+        else
+            return false;
+        
     }
-    int i=dp[0][n-1];
-    int s=0;
-    for(int j=0;j<n;j++){
-        s+=a[j];
-    }
-    if(i>=s/2.0) return 1;
-    return 0;
-}
 };
