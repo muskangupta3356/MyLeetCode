9acc3966cf2ff6f0ff915333dd1324e21891afa7
muskangupta3356 <muskangupta270798@gmail.com>
2022-11-23 13:06:35 +0530

    Time: 5 ms (25.00%), Space: 5.7 MB (95.73%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int res=0;
+        while(target>startValue)
+        {
+           target=(target%2==0)?target/2:target+1;
+           res++;
+        }
+        res=res+(startValue-target);
+        return res;
+    }
+};
