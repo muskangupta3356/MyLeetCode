671c4821ebdd17076114e0167ce25e7341499a7d
muskangupta3356 <muskangupta270798@gmail.com>
2024-04-16 01:40:38 +0530

    Time: 1 ms (20.17%), Space: 43.3 MB (9.90%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,10 +2,13 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
         int peackIndex =-1;
-        for(int i=1;i<n;i++)
+        for(int i=n-2;i>=0;i--)
         {
-            if(nums[i]>nums[i-1])
+            if(nums[i]<nums[i+1]){
+                
                 peackIndex=i;
+                break;
+            }
         }
         
         if(peackIndex==-1)
@@ -14,18 +17,19 @@ class Solution {
             return;
         }
          int index=peackIndex;
-        for(int i=peackIndex+1;i<n;i++)
+        for(int i=n-1;i>peackIndex;i--)
         {
-            if(nums[i]>nums[peackIndex-1] && nums[i]<nums[peackIndex])
+            if(nums[i]>nums[peackIndex])
             {
                 index=i;
+                break;
             }
         }
         //swap(nums[peackIndex-1],nums[index]);
-        int temp=nums[peackIndex-1];
-        nums[peackIndex-1]=nums[index];
+        int temp=nums[peackIndex];
+        nums[peackIndex]=nums[index];
         nums[index]=temp;
-        Arrays.sort(nums,peackIndex,nums.length);
+        Arrays.sort(nums,peackIndex+1,nums.length);
         return;
     }
 }
